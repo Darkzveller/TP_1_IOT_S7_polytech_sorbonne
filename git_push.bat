3470347f7856d03ad850d90583f5fd6469144125
Darkzveller <elkattoufi.youssef@gmail.com>
2026-09-13 18:21:06 +0200

    Correction du desktop ini pour juste prendre en compte le desktop ini

--- git_push.bat
+++ git_push.bat
@@ -1,2 +1,2 @@
 echo off
-git status && git add . && git commit -m "maj" && git push
+git status && git add . && git commit -m "Correction du desktop ini pour juste prendre en compte le desktop ini" && git push
